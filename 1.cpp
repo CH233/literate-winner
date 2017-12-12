f0992470aa1b476a4fa5754004b029d4df5b65d0
CH233 <31720310+CH233@users.noreply.github.com>
2017-12-12 18:01:04 +0800

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,43 +1,24 @@
-#include <iostream.h>
-#include <iomanip.h>
-#include <stdio.h>
-#define N 10
+#include<iostream.h>
+#include<iomanip.h>
 void main()
 {
-	int i;
-	int a[N];
-	void inv(int *p,int n);
-	cout<<"请输入10个整数："<<endl;
-	for(i=0;i<N;i++)
-		cin>>a[i];
-	cout<<"此数组是："<<endl;
-	for(i=0;i<N;i++)
-		cout<<setw(3)<<a[i];
-	cout<<"\n";
-	inv(a,10);
-	cout<<"交换元素后的数组是："<<endl;
-	for(i=0;i<N;i++)
-		cout<<setw(3)<<a[i];
-	cout<<"\n";
-}
-void inv(int *p,int n)
+int a[10];
+int i,t,*p,*q;
+cout<<"请输入10个数:";
+for(i=0;i<10;i++)
+cin>>a[i];
+p=a;
+q=&a[9];
+while(p<q)
 {
-	int i,max,min,*pmax,*pmin,temp;
-	
-	for(max=min=p[0],i=0;i<n;i++)
-	{
-		if(max<=*(p+i))
-		{
-			max=*(p+i);
-		pmax=p+i;
-		}
-		if(min>=*(p+i))
-		{
-			min=*(p+i);
-		pmin=p+i;
-		}
-	}
-	temp=*pmax;
-	*pmax=*pmin;
-	*pmin=temp;
+t=*p;
+*p=*q;
+*q=t;
+p++;
+q--;
+}
+cout<<"反序输出为:";
+for(i=0;i<10;i++)
+cout<<setw(3)<<a[i];
+cout<<endl;
 }
